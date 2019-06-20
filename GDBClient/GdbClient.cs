9d1cb3e1177ff507235c43907dacc5ced4f93de6
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-06-21 01:02:53 +0200

    Misc fixes with memory access
    No false positive errors.
    No space in write command.

--- GDBClient/GdbClient.cs
+++ GDBClient/GdbClient.cs
@@ -51,7 +51,7 @@ namespace Henke37.DebugHelp.Gdb {
 		public override void ReadBytes(uint addr, uint size, byte[] readBuff) {
 			string reply = SendCommand($"m{addr:x},{size:x}");
 			if(reply == "") throw new UnsupportedCommandException();
-			if(reply.StartsWith("E")) {
+			if(reply.Length==3 && reply.StartsWith("E")) {
 				GDBException.ThrowFromReply(reply);
 			}
 
@@ -68,7 +68,7 @@ namespace Henke37.DebugHelp.Gdb {
 
 		public override void WriteBytes(byte[] srcBuff, uint dstAddr, uint size) {
 			var sb = new StringBuilder();
-			sb.AppendFormat($"M {0:x},{1:x}:", dstAddr, size);
+			sb.AppendFormat($"M{0:x},{1:x}:", dstAddr, size);
 
 			for(int i = 0; i < size; ++i) {
 				sb.AppendFormat("{0:x2}", srcBuff[i]);
