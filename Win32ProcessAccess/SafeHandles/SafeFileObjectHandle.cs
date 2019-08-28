1272e3eb0eb1045a86db93534c6bb99bff57400e
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-08-28 12:22:41 +0200

    New SafeFileObjectHandle

---
+++ Win32ProcessAccess/SafeHandles/SafeFileObjectHandle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Henke37.DebugHelp.Win32.SafeHandles {
+	internal class SafeFileObjectHandle : SafeKernelObjHandle, IEquatable<SafeFileObjectHandle> {
+		internal static readonly SafeFileObjectHandle InvalidHandle=new SafeFileObjectHandle(InvalidHandleValue,false);
+
+		internal SafeFileObjectHandle(IntPtr handle, bool ownsHandle) : base(handle, ownsHandle) {
+		}
+
+		public bool Equals(SafeFileObjectHandle other) {
+			if(other.handle == handle) return true;
+			return CompareObjectHandles(other.handle, handle);
+		}
+	}
+}
