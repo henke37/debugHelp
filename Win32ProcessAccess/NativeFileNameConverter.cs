d8cb748577c070e2e5443b97ad52e25f661daad2
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-08-29 00:25:17 +0200

    Trim unused using lines

--- Win32ProcessAccess/NativeFileNameConverter.cs
+++ Win32ProcessAccess/NativeFileNameConverter.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Henke37.DebugHelp.Win32 {
 	public class NativeFileNameConverter {
