5ce91799e740e1b4c308cd92c493c02fcee0c249
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-08-28 13:30:42 +0200

    MemoryNotification support

---
+++ Win32ProcessAccess/MemoryNotification.cs
@@ -0,0 +1,34 @@
+﻿using Henke37.DebugHelp.Win32.SafeHandles;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace Henke37.DebugHelp.Win32 {
+	public class MemoryNotification {
+		SafeMemoryNotificationHandle handle;
+
+		public MemoryNotification(MemoryNotificationType type) {
+			handle= CreateMemoryResourceNotification((uint)type);
+			if(handle.IsInvalid) throw new Win32Exception();
+		}
+
+		public bool Status {
+			get {
+				var success = QueryMemoryResourceNotification(handle, out bool status);
+				if(!success) throw new Win32Exception();
+				return status;
+			}
+		}
+
+		[DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true)]
+		internal static unsafe extern SafeMemoryNotificationHandle CreateMemoryResourceNotification(uint type);
+
+		[DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		internal static unsafe extern bool QueryMemoryResourceNotification(SafeMemoryNotificationHandle handle, [MarshalAs(UnmanagedType.Bool)] out bool status);
+	}
+
+	public enum MemoryNotificationType : uint {
+		LowMemory=0,
+		HighMemory=1
+	}
+}
