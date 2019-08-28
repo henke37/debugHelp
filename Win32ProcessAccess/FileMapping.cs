2eff8172b0abb9d5d0b991cc0f5ffde4d0970019
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-08-28 12:24:41 +0200

    FileMapping should be IDisposable

--- Win32ProcessAccess/FileMapping.cs
+++ Win32ProcessAccess/FileMapping.cs
@@ -4,20 +4,24 @@ using System.ComponentModel;
 using System.Runtime.InteropServices;
 
 namespace Henke37.DebugHelp.Win32 {
-	public class FileMapping {
+	public class FileMapping : IDisposable {
 		private SafeFileMappingHandle handle;
 
 		internal FileMapping(SafeFileMappingHandle handle) {
 			this.handle = handle;
 		}
 
-		public static unsafe FileMapping CreateVirtualMapping(MemoryProtection memProtection,uint high, uint low, string name) {
-			SafeFileMappingHandle handle = CreateFileMappingA(SafeFileObjectHandle.InvalidHandle, null,(uint)memProtection,high,low,name);
+		public static unsafe FileMapping CreateVirtualMapping(MemoryProtection memProtection, uint high, uint low, string name) {
+			SafeFileMappingHandle handle = CreateFileMappingA(SafeFileObjectHandle.InvalidHandle, null, (uint)memProtection, high, low, name);
 			if(handle.IsInvalid) throw new Win32Exception();
 			return new FileMapping(handle);
 		}
 
 		[DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true, CharSet = CharSet.Ansi)]
-		internal static unsafe extern SafeFileMappingHandle CreateFileMappingA(SafeFileObjectHandle fileHandle, [Optional] SecurityAttributes *securityAttributes, UInt32 flProtect, UInt32 dwMaximumSizeHigh, UInt32 dwMaximumSizeLow, [MarshalAs(UnmanagedType.LPStr)] string name);
+		internal static unsafe extern SafeFileMappingHandle CreateFileMappingA(SafeFileObjectHandle fileHandle, [Optional] SecurityAttributes* securityAttributes, UInt32 flProtect, UInt32 dwMaximumSizeHigh, UInt32 dwMaximumSizeLow, [MarshalAs(UnmanagedType.LPStr)] string name);
+
+		public void Dispose() {
+			((IDisposable)handle).Dispose();
+		}
 	}
 }
