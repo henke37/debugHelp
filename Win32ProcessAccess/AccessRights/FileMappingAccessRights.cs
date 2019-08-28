591265adadbd38d8b47822ff2b0965ede0855720
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-08-28 12:48:06 +0200

    Basic support for OpenMapping

---
+++ Win32ProcessAccess/AccessRights/FileMappingAccessRights.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Henke37.DebugHelp.Win32.AccessRights {
+	[Flags]
+	public enum FileMappingAccessRights : uint {
+		None=0,
+		Execute = 0x0020,
+		Read=4,
+		Write=2,
+		All=Read|Write|Execute
+	}
+}
--- Win32ProcessAccess/FileMapping.cs
+++ Win32ProcessAccess/FileMapping.cs
@@ -1,4 +1,5 @@
-﻿using Henke37.DebugHelp.Win32.SafeHandles;
+﻿using Henke37.DebugHelp.Win32.AccessRights;
+using Henke37.DebugHelp.Win32.SafeHandles;
 using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
@@ -23,8 +24,17 @@ namespace Henke37.DebugHelp.Win32 {
 			return new FileMapping(handle);
 		}
 
+		public static unsafe FileMapping OpenMapping(string name,bool inherit=false, FileMappingAccessRights accessRights=FileMappingAccessRights.All) {
+			SafeFileMappingHandle handle = OpenFileMappingA((uint)accessRights,inherit,name);
+			if(handle.IsInvalid) throw new Win32Exception();
+			return new FileMapping(handle);
+		}
+
 		[DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true, CharSet = CharSet.Ansi)]
 		internal static unsafe extern SafeFileMappingHandle CreateFileMappingA(SafeFileObjectHandle fileHandle, [Optional] SecurityAttributes* securityAttributes, UInt32 flProtect, UInt32 dwMaximumSizeHigh, UInt32 dwMaximumSizeLow, [MarshalAs(UnmanagedType.LPStr)] string name);
+
+		[DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true, CharSet = CharSet.Ansi)]
+		internal static unsafe extern SafeFileMappingHandle OpenFileMappingA(UInt32 access, [MarshalAs(UnmanagedType.Bool)] bool inherit, [MarshalAs(UnmanagedType.LPStr)] string name);
 
 		public void Dispose() {
 			((IDisposable)handle).Dispose();
