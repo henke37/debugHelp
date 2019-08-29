2047ad9f05ddbe79784b1e8a574621a62850944d
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-08-29 23:12:43 +0200

    The absolute minimum for a NativeFileObject class

---
+++ Win32ProcessAccess/NativeFileObject.cs
@@ -0,0 +1,15 @@
+﻿using Henke37.DebugHelp.Win32.SafeHandles;
+using System;
+
+namespace Henke37.DebugHelp.Win32 {
+	public class NativeFileObject : IDisposable {
+		internal SafeFileObjectHandle handle;
+
+		internal NativeFileObject(SafeFileObjectHandle handle) {
+			this.handle = handle;
+		}
+
+		public void Dispose() => handle.Dispose();
+		public void Close() => handle.Close();
+	}
+}
