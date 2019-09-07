e90ab5c917671e22c9006c5a04f8e21634719576
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-09-07 02:59:02 +0200

    Should be IDisposable

--- Win32ProcessAccess/NativeToken.cs
+++ Win32ProcessAccess/NativeToken.cs
@@ -1,11 +1,16 @@
 ﻿using Henke37.DebugHelp.Win32.SafeHandles;
+using System;
 
 namespace Henke37.DebugHelp.Win32 {
-	public class NativeToken {
+	public class NativeToken : IDisposable {
 		private SafeTokenHandle tokenHandle;
 
 		internal NativeToken(SafeTokenHandle tokenHandle) {
 			this.tokenHandle = tokenHandle;
+		}
+
+		public void Dispose() {
+			tokenHandle.Dispose();
 		}
 	}
 }
