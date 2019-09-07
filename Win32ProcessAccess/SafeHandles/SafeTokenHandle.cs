e68dca3d910330e7e1f39791928d1276f1b62a13
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-09-07 02:30:22 +0200

    Basic SafeTokenHandle class.

---
+++ Win32ProcessAccess/SafeHandles/SafeTokenHandle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Henke37.Win32.Base.SafeHandles;
+namespace Henke37.DebugHelp.Win32.SafeHandles {
+	internal class SafeTokenHandle : SafeKernelObjHandle, IEquatable<SafeTokenHandle> {
+		private SafeTokenHandle() : base(true) {
+		}
+
+		public bool Equals(SafeTokenHandle other) {
+			return CompareObjectHandles(handle, other.handle);
+		}
+	}
+}
