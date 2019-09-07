4998f1e4bca1064b03d737df1948cf3d9f4f4dab
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-09-07 12:37:11 +0200

    Support NtCompareTokens

--- Win32ProcessAccess/NativeToken.cs
+++ Win32ProcessAccess/NativeToken.cs
@@ -1,8 +1,9 @@
 ﻿using Henke37.DebugHelp.Win32.SafeHandles;
 using System;
+using System.Runtime.InteropServices;
 
 namespace Henke37.DebugHelp.Win32 {
-	public class NativeToken : IDisposable {
+	public class NativeToken : IDisposable, IEquatable<NativeToken> {
 		private SafeTokenHandle tokenHandle;
 
 		internal NativeToken(SafeTokenHandle tokenHandle) {
@@ -12,5 +13,16 @@ namespace Henke37.DebugHelp.Win32 {
 		public void Dispose() {
 			tokenHandle.Dispose();
 		}
+
+		public bool Equals(NativeToken other) {
+			var status = NtCompareTokens(tokenHandle, other.tokenHandle, out bool equal);
+			if(status.Severity!=PInvoke.NTSTATUS.SeverityCode.STATUS_SEVERITY_SUCCESS) {
+				throw new PInvoke.NTStatusException(status);
+			}
+			return equal;
+		}
+
+		[DllImport("Ntdll.dll", ExactSpelling = true, SetLastError = true)]
+		internal static extern unsafe PInvoke.NTSTATUS NtCompareTokens(SafeTokenHandle handle1, SafeTokenHandle handle2, [MarshalAs(UnmanagedType.Bool)] out bool equal);
 	}
 }
