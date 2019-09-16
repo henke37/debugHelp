d20024aad98dcb410de23d2326e2f06f8765c2af
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-09-16 20:51:18 +0200

    Be sure to register the com callbacks for EVT.

--- Win32WaitChain/WaitChainSession.cs
+++ Win32WaitChain/WaitChainSession.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PInvoke;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
@@ -7,6 +8,10 @@ using System.Threading.Tasks;
 namespace Henke37.Win32.WaitChain {
 	public class WaitChainSession {
 		internal SafeWaitChainSessionHandle handle;
+
+		static WaitChainSession() {
+			registerCallbacks();
+		}
 
 		public unsafe WaitChainSession() {
 			handle = OpenThreadWaitChainSession(
@@ -27,6 +32,10 @@ namespace Henke37.Win32.WaitChain {
 			}
 		}
 
+		private static void registerCallbacks() {
+			RegisterWaitChainCOMCallback(CoGetCallState, CoGetActivationState);
+		}
+
 		internal unsafe delegate void Pwaitchaincallback(
 		  SafeWaitChainSessionHandle WctHandle,
 		  IntPtr Context,
@@ -40,5 +49,21 @@ namespace Henke37.Win32.WaitChain {
 		internal static extern SafeWaitChainSessionHandle OpenThreadWaitChainSession(UInt32 flags, Pwaitchaincallback callback);
 
 		enum WCT_SESSION_OPEN_FLAGS : uint { WCT_SYNC_OPEN_FLAG = 0, WCT_ASYNC_OPEN_FLAG = 1 };
+
+		internal delegate HResult PCOGETCALLSTATE(int x,ref UInt32 y);
+		internal delegate HResult PCOGETACTIVATIONSTATE(Guid g,UInt32 x,ref UInt32 y);
+
+		[DllImport("Advapi32.dll", ExactSpelling = true, SetLastError = true)]
+		internal static extern void RegisterWaitChainCOMCallback(
+		  PCOGETCALLSTATE CallStateCallback,
+		  PCOGETACTIVATIONSTATE ActivationStateCallback
+		);
+
+
+		[DllImport("ole32.dll", ExactSpelling = true, SetLastError = true)]
+		internal static extern HResult CoGetCallState(int x, ref UInt32 y);
+
+		[DllImport("ole32.dll", ExactSpelling = true, SetLastError = true)]
+		internal static extern HResult CoGetActivationState(Guid g, UInt32 x, ref UInt32 y);
 	}
 }
