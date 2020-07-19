aad86c9b498d4027c8ca9d3e1915ffb798e5ffc9
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-07-20 00:36:22 +0200

    Skeleton for SetInformationJobObject

--- Win32ProcessAccess/NativeJob.cs
+++ Win32ProcessAccess/NativeJob.cs
@@ -66,6 +66,13 @@ namespace Henke37.DebugHelp.Win32 {
 			}
 		}
 
+		internal unsafe void SetInformationJob<T>(JobInformationClass infoClass, ref T buff) where T : unmanaged {
+			fixed(void* buffP = &buff) {
+				bool success = SetInformationJobObject(handle, infoClass, buffP, (uint)sizeof(T));
+				if(!success) throw new Win32Exception();
+			}
+		}
+
 		[SecuritySafeCritical]
 		void Terminate(UInt32 exitCode) {
 			TerminateJobObject(handle, exitCode);
@@ -86,6 +93,11 @@ namespace Henke37.DebugHelp.Win32 {
 		[DllImport("Ntdll.dll", ExactSpelling = true, SetLastError = true)]
 		[return: MarshalAs(UnmanagedType.Bool)]
 		internal static extern unsafe bool QueryInformationJobObject(SafeJobHandle handle, JobInformationClass informationClass, void* buffer, uint bufferLength, out uint returnLength);
+
+
+		[DllImport("Ntdll.dll", ExactSpelling = true, SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		internal static extern unsafe bool SetInformationJobObject(SafeJobHandle handle, JobInformationClass informationClass, void* buffer, uint bufferLength);
 
 		[DllImport("Ntdll.dll", ExactSpelling = true, SetLastError = true)]
 		[return: MarshalAs(UnmanagedType.Bool)]
