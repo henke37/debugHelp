ff95068f568145936e27752f09b722d80c36d251
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-07-19 23:22:56 +0200

    Add SafeJobHandle class

---
+++ Win32ProcessAccess/SafeHandles/SafeJobHandle.cs
@@ -0,0 +1,24 @@
+﻿using Henke37.Win32.Base.SafeHandles;
+using System;
+
+namespace Henke37.DebugHelp.Win32.SafeHandles {
+	class SafeJobHandle : SafeKernelObjHandle, IEquatable<SafeJobHandle> {
+		private SafeJobHandle() : base(true) {
+		}
+
+		internal SafeJobHandle(IntPtr handle, bool ownsHandle = true) : base(handle, ownsHandle) {
+		}
+
+		internal static SafeJobHandle DuplicateFrom(IntPtr handle) {
+			return new SafeJobHandle(DuplicateHandleLocal(handle, 0, false, DuplicateOptions.SameAccess), true);
+		}
+		internal static SafeJobHandle DuplicateFrom(IntPtr handle, uint accessRights) {
+			return new SafeJobHandle(DuplicateHandleLocal(handle, accessRights, false, DuplicateOptions.None), true);
+		}
+
+		public bool Equals(SafeJobHandle other) {
+			if(other.handle == handle) return true;
+			return CompareObjectHandles(other.handle, handle);
+		}
+	}
+}
