011d830c7d8766dbbb309a4d2f6f544644d5786d
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-07-20 22:43:35 +0200

    Minimal test program for Jobs

---
+++ JobTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using Henke37.DebugHelp.Win32.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTest {
+	class Program {
+		static void Main(string[] args) {
+
+			using(NativeJob job = NativeJob.Create()) {
+
+			}
+			
+		}
+	}
+}
