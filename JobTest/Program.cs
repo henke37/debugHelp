ca76b74425842e7fa55a798403f6da503350d910
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-07-21 03:55:39 +0200

    Simple process limiting

--- JobTest/Program.cs
+++ JobTest/Program.cs
@@ -2,6 +2,7 @@
 using Henke37.DebugHelp.Win32.Jobs;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,19 @@ using System.Threading.Tasks;
 namespace JobTest {
 	class Program {
 		static void Main(string[] args) {
-
 			using(NativeJob job = NativeJob.Create()) {
 				var extended = job.ExtendedLimitInformation;
-
 				extended.LimitFlags = LimitFlags.JobTime | LimitFlags.JobMemory;
 				extended.PerJobUserTimeLimit = new TimeSpan(0, 5, 0);
 				extended.JobMemoryLimit = 1024 * 1024 * 100;
 				job.ExtendedLimitInformation = extended;
+
+				if(args.Length >= 1) {
+					using(Process process = Process.Start(args[0]))
+					using(NativeProcess natProcess = NativeProcess.FromProcess(process)) {
+						job.AttachProcess(natProcess);
+					}
+				}
 			}
 			
 		}
