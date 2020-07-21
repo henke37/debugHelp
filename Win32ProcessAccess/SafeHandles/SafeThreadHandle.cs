f7e081e960571d9db6e8868f26207e1f151376bd
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-07-21 14:12:39 +0200

    CurrentThread property for SafeThreadHandle

--- Win32ProcessAccess/SafeHandles/SafeThreadHandle.cs
+++ Win32ProcessAccess/SafeHandles/SafeThreadHandle.cs
@@ -1,16 +1,35 @@
 ﻿using Henke37.Win32.Base.SafeHandles;
 using System;
+using System.Security.Permissions;
 
 namespace Henke37.DebugHelp.Win32.SafeHandles {
 	internal sealed class SafeThreadHandle : SafeKernelObjHandle, IEquatable<SafeThreadHandle> {
+		internal SafeThreadHandle(IntPtr handle, bool ownsHandle = true) : base(handle, ownsHandle) {
+		}
 		internal SafeThreadHandle(IntPtr handle) : base(handle, true) {
 		}
 		private SafeThreadHandle() : base(true) {
 		}
 
+		public static SafeThreadHandle CurrentThread {
+#if NETFRAMEWORK
+			[HostProtection(SelfAffectingThreading = true)]
+#endif
+			get {
+				return new SafeThreadHandle((IntPtr)(-2), false);
+			}
+		}
+
 		public bool Equals(SafeThreadHandle other) {
 			if(other.handle == handle) return true;
 			return CompareObjectHandles(other.handle, handle);
+		}
+
+		public override bool IsInvalid {
+			get {
+				if(handle.ToInt32() == -2) return false;
+				return base.IsInvalid;
+			}
 		}
 	}
 }
