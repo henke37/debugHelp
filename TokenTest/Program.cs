e00cc691db4a29f0188797f43e7f52c44509cbb5
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-07-23 01:55:32 +0200

    TokenTest project

---
+++ TokenTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using Henke37.Win32.Processes;
+using Henke37.Win32.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokenTest {
+	class Program {
+		static void Main(string[] args) {
+			using(NativeToken token=NativeProcess.Current.OpenToken(System.Security.Principal.TokenAccessLevels.Query)) {
+				var privs = token.Privileges;
+			}
+		}
+	}
+}
