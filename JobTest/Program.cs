df832f0567d597b545df309924663d049ba5b7d9
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-09-02 14:28:24 +0200

    Test code for attribute lists

--- JobTest/Program.cs
+++ JobTest/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Henke37.Win32.Jobs;
 using Henke37.Win32.Processes;
+using Henke37.Win32.Snapshots;
+using Henke37.Win32.AccessRights;
 using System;
+using System.Linq;
 
 namespace JobTest {
 	class Program {
@@ -13,12 +16,24 @@ namespace JobTest {
 				job.ExtendedLimitInformation = extended;
 
 				if(args.Length >= 1) {
-					using(NativeProcess natProcess = NativeProcess.CreateProcess(args[0],null, CreateProcessFlags.None, StartupInfoFlags.None, null, out _)) {
-						job.AttachProcess(natProcess);
+
+					using(var atts = new ProcThreadAttributeList(1)) {
+
+						var shellProc = GetShellProcess();
+
+						using(NativeProcess natProcess = NativeProcess.CreateProcess(args[0], null, CreateProcessFlags.None, StartupInfoFlags.None, atts, null, out _)) {
+							job.AttachProcess(natProcess);
+						}
 					}
 				}
 			}
 			
+		}
+
+		static NativeProcess GetShellProcess() {
+			using(var snap = new Toolhelp32Snapshot(Toolhelp32SnapshotFlags.Process)) {
+				return snap.GetProcesses().Where(p => p.Executable == "explorer.exe").First().Open(ProcessAccessRights.All);
+			}
 		}
 	}
 }
