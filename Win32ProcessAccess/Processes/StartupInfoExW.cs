954ad3146a91afbd6a1fae80efc11640eb7d2a99
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-09-02 14:40:31 +0200

    Trim unused using statements

--- Win32ProcessAccess/Processes/StartupInfoExW.cs
+++ Win32ProcessAccess/Processes/StartupInfoExW.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Henke37.Win32.Processes {
+﻿namespace Henke37.Win32.Processes {
 	internal unsafe struct StartupInfoExW {
 		public StartupInfoW StartupInfo;
 		public ProcThreadAttributeList.Native *attributeList;
