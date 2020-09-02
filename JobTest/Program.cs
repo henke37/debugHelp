76c83a67fc5bffd7fb434dfb1b13277137e93649
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-09-02 14:39:00 +0200

    Working parent process override

--- JobTest/Program.cs
+++ JobTest/Program.cs
@@ -7,7 +7,7 @@ using System.Linq;
 
 namespace JobTest {
 	class Program {
-		static void Main(string[] args) {
+		static unsafe void Main(string[] args) {
 			using(NativeJob job = NativeJob.Create()) {
 				var extended = job.ExtendedLimitInformation;
 				extended.LimitFlags = LimitFlags.JobTime | LimitFlags.JobMemory;
@@ -21,6 +21,10 @@ namespace JobTest {
 
 						var shellProc = GetShellProcess();
 
+						IntPtr shellHandle = shellProc.Handle.DangerousGetHandle();
+
+						atts.AddAttribute<IntPtr>(ProcThreadAttribute.ParentProcess, &shellHandle);
+
 						using(NativeProcess natProcess = NativeProcess.CreateProcess(args[0], null, CreateProcessFlags.None, StartupInfoFlags.None, atts, null, out _)) {
 							job.AttachProcess(natProcess);
 						}
@@ -32,7 +36,7 @@ namespace JobTest {
 
 		static NativeProcess GetShellProcess() {
 			using(var snap = new Toolhelp32Snapshot(Toolhelp32SnapshotFlags.Process)) {
-				return snap.GetProcesses().Where(p => p.Executable == "explorer.exe").First().Open(ProcessAccessRights.All);
+				return snap.GetProcesses().Where(p => p.Executable == "explorer.exe").First().Open(ProcessAccessRights.CreateProcess);
 			}
 		}
 	}
