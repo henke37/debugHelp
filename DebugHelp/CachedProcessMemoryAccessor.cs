cf1a102d6edb2fe124e78ac5f74713a3504b489a
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-01-04 15:27:00 +0100

    New CachedProcessMemoryAccessor class

---
+++ DebugHelp/CachedProcessMemoryAccessor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Henke37.DebugHelp {
+	public class CachedProcessMemoryAccessor : ProcessMemoryAccessor {
+
+		private Dictionary<IntPtr, byte[]> pageCache;
+		private ProcessMemoryAccessor realAccessor;
+
+		private const int pageSize = 4096;
+
+		public CachedProcessMemoryAccessor(ProcessMemoryAccessor realAccessor) {
+			this.realAccessor = realAccessor ?? throw new ArgumentNullException(nameof(realAccessor));
+			pageCache = new Dictionary<IntPtr, byte[]>();
+		}
+
+		public void ClearCache() {
+			pageCache.Clear();
+		}
+
+		public void ClearCache(IntPtr pageAddr) {
+			pageCache.Remove(pageAddr);
+		}
+
+		private IntPtr BasePageAddress(IntPtr address, out int offset) {
+			offset = address.ToInt32() % pageSize;
+			return (IntPtr)(address.ToInt64() & ~(pageSize-1));
+		}
+
+		public override void ReadBytes(IntPtr addr, uint size, byte[] buff) {
+			var pageAddr = BasePageAddress(addr, out int offset);
+			for(int copiedData = 0; copiedData < size;) {
+				byte[] readPage;
+				if(!pageCache.TryGetValue(pageAddr, out readPage)) {
+					readPage=realAccessor.ReadBytes(pageAddr, pageSize);
+					pageCache[pageAddr] = readPage;
+				}
+
+				int remainingBytes = (int)size - copiedData;
+				int copySize = remainingBytes > pageSize ? pageSize : remainingBytes;
+				Array.Copy(readPage, offset, buff, copiedData, copySize);
+
+				offset = 0;
+				pageAddr += pageSize;
+				copiedData += copySize;
+			}
+		}
+
+		public override void WriteBytes(byte[] srcBuff, IntPtr dstAddr, uint size) {
+			var pageAddr = BasePageAddress(dstAddr, out _);
+			var endPageAddr = BasePageAddress(dstAddr + (int)size, out _);
+			for(;pageAddr!=endPageAddr;pageAddr+=pageSize) {
+				ClearCache(pageAddr);
+			}
+
+			realAccessor.WriteBytes(srcBuff, dstAddr, size);
+		}
+	}
+}
