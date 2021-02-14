98ed132d4cb31ab4ed3f87049170ae5085435b86
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-02-14 23:48:59 +0100

    No longer hardcode the process id

--- UnloadedModuleTest/Program.cs
+++ UnloadedModuleTest/Program.cs
@@ -5,7 +5,7 @@ using Henke37.Win32.LastUnloadedModules;
 namespace UnloadedModuleTest {
 	class Program {
 		static void Main(string[] args) {
-			var proc = NativeProcess.Open(24032);
+			var proc = NativeProcess.Open(uint.Parse(args[0]));
 			var procMem = new LiveProcessMemoryAccessor(proc);
 			var unloaded = new UnloadedModulesAccessor(procMem);
 
