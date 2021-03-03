cd71a6b5d72662f20100d7daa711f8eb1d3bd0d7
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-03-03 15:26:01 +0100

    Working basic restart manager.

--- Win32RestartManager/RestartManager.cs
+++ Win32RestartManager/RestartManager.cs
@@ -8,17 +8,22 @@ using System.Threading.Tasks;
 namespace Henke37.Win32.Restart {
 	public class RestartManager : IDisposable {
 
-		private RestartManagerHandle handle;
+		private UIntPtr handle;
+		private bool disposedValue;
 
-		public RestartManager(string sessionKey) {
-			var result = RmStartSession(out handle, 0, sessionKey);
+		public RestartManager(out string sessionKey) {
+			var sb = new StringBuilder();
+			sb.Capacity = 128;
+			var result = RmStartSession(out handle, 0, sb);
+			sessionKey = sb.ToString();
 			CheckResult(result);
 		}
 
-		private RestartManager(RestartManagerHandle handle) { this.handle = handle; }
+		private RestartManager(UIntPtr handle) { this.handle = handle; }
 
 		public static RestartManager JoinSession(string sessionKey) {
 			var result = RmJoinSession(out var handle, sessionKey);
+
 			CheckResult(result);
 			return new RestartManager(handle);
 		}
@@ -27,14 +32,37 @@ namespace Henke37.Win32.Restart {
 			if(result != RMResult.Success) throw new Exception();
 		}
 
-		public void Dispose() {
-			((IDisposable)handle).Dispose();
+		[DllImport("Rstrtmgr.dll", ExactSpelling = true, SetLastError = true, CharSet = CharSet.Unicode)]
+		private static extern RMResult RmStartSession(out UIntPtr handle, UInt32 flags, [MarshalAs(UnmanagedType.LPWStr)] StringBuilder sessionKey);
+
+		[DllImport("Rstrtmgr.dll", ExactSpelling = true, SetLastError = true)]
+		private static extern RMResult RmJoinSession(out UIntPtr handle, [MarshalAs(UnmanagedType.LPWStr)] string sessionKey);
+
+		[DllImport("Rstrtmgr.dll", ExactSpelling = true, SetLastError = true)]
+		private static extern RMResult RmEndSession(UIntPtr handle);
+
+		protected virtual void Dispose(bool disposing) {
+			if(!disposedValue) {
+				if(disposing) {
+					// TODO: dispose managed state (managed objects)
+				}
+
+				RmEndSession(handle);
+				// TODO: set large fields to null
+				disposedValue = true;
+			}
 		}
 
-		[DllImport("Rstrtmgr.dll", ExactSpelling = true, SetLastError = true)]
-		private static extern RMResult RmStartSession(out RestartManagerHandle handle, UInt32 flags, [MarshalAs(UnmanagedType.LPWStr)] string sessionKey);
+		// // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+		~RestartManager() {
+		    // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		     Dispose(disposing: false);
+		}
 
-		[DllImport("Rstrtmgr.dll", ExactSpelling = true, SetLastError = true)]
-		private static extern RMResult RmJoinSession(out RestartManagerHandle handle, [MarshalAs(UnmanagedType.LPWStr)] string sessionKey);
+		public void Dispose() {
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
 	}
 }
