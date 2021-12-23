0634686c472fc6e18a2bad8fe675dc7979a3f97e
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-12-23 21:59:19 +0100

    Fill in enums with values

--- Win32Debug/ExceptionFlag.cs
+++ Win32Debug/ExceptionFlag.cs
@@ -3,7 +3,13 @@
 namespace Henke37.Win32.Debug {
 	[Flags]
 	public enum ExceptionFlag {
-		Noncontinuable,
-		SoftwareOriginate
+		Noncontinuable=1,
+		Unwinding = 2,
+		ExitUnwind = 4,
+		StackInvalid = 8,
+		NestedCall = 0x10,
+		TargetUnwind = 0x20,
+		CollidedUnwind = 0x40,
+		UnwindMask = 0x66
 	}
 }
