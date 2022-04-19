1be69337100c2baf68576f7e3037248acb0b8cd6
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2022-04-19 16:50:06 +0200

    Extension methods for ThreadEntry classes

--- Win32Windows/WindowExtensions.cs
+++ Win32Windows/WindowExtensions.cs
@@ -1,14 +1,16 @@
 ﻿using Henke37.Win32.Threads;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Henke37.Win32.Windows {
 	public static class WindowExtensions {
 		public static List<NativeWindow> GetWindows(this NativeThread thread) {
+			return NativeWindow.GetThreadWindows(thread.ThreadId);
+		}
+
+		public static List<NativeWindow> GetWindows(this Snapshots.ThreadEntry thread) {
+			return NativeWindow.GetThreadWindows(thread.ThreadId);
+		}
+		public static List<NativeWindow> GetWindows(this Clone.QueryStructs.ThreadEntry thread) {
 			return NativeWindow.GetThreadWindows(thread.ThreadId);
 		}
 	}
