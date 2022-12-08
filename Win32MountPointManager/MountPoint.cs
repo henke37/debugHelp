10880374a535c93811f3d967098512c73af46bf0
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2022-12-08 22:16:39 +0100

    Basic stub for MountPointManager

---
+++ Win32MountPointManager/MountPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Henke37.Win32.MountPointManager {
+	public class MountPoint {
+		public string? SymbolicLinkName;
+		public string? UniqueId;
+		public string? DeviceName;
+
+		internal struct Header {
+			internal UInt32 SymLinkNameOffset;
+			internal UInt16 SymLinkNameLength;
+			internal UInt16 Padding1;
+
+			internal UInt32 UniqueIdOffset;
+			internal UInt16 UniqueIdLength;
+			internal UInt16 Padding2;
+
+			internal UInt32 DeviceNameOffset;
+			internal UInt16 DeviceNameLength;
+			internal UInt16 Padding3;
+		}
+	}
+}
---
+++ Win32MountPointManager/MountPointManager.cs
@@ -0,0 +1,26 @@
+﻿using Henke37.Win32.Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Henke37.Win32.MountPointManager {
+	public class MountPointManager {
+		private NativeFileObject file;
+
+		public MountPointManager() {
+			file = NativeFileObject.Open(
+				"\\\\.\\MountPointManager", 
+				AccessRights.FileObjectAccessRights.GenericRead,
+				FileShareMode.Read,
+				FileDisposition.OpenExisting, 
+				FileAttributes.Normal
+			);
+		}
+
+		public List<MountPoint> QueryPoints(MountPoint needle) {
+			throw new NotImplementedException();
+		}
+	}
+}
