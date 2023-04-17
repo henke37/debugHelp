0af24c09066d7a6d0d38d40aec0b383b31717fe0
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2023-04-17 22:42:27 +0200

    Enum for object attributes

--- Win32Base/SafeHandles/PublicObjectBasicInformation.cs
+++ Win32Base/SafeHandles/PublicObjectBasicInformation.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 namespace Henke37.Win32.SafeHandles {
 	[Undocumented]
 	public class PublicObjectBasicInformation {
-		public UInt32 Attributes;
+		public ObjectAttributes Attributes;
 		public UInt32 GrantedAccess;
 		public UInt32 HandleCount;
 		public UInt32 PointerCount;
@@ -13,7 +13,7 @@ namespace Henke37.Win32.SafeHandles {
 		[Undocumented]
 		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
 		internal unsafe struct Native {
-			UInt32 Attributes;
+            UInt32 Attributes;
 			UInt32 GrantedAccess;
 			UInt32 HandleCount;
 			UInt32 PointerCount;
@@ -21,12 +21,26 @@ namespace Henke37.Win32.SafeHandles {
 
 			public PublicObjectBasicInformation AsManaged() {
 				return new PublicObjectBasicInformation() {
-					Attributes    = Attributes,
+					Attributes    = ((ObjectAttributes)Attributes),
 					GrantedAccess = GrantedAccess,
 					HandleCount   = HandleCount,
 					PointerCount  = PointerCount
 				};
 			}
 		}
+	}
+
+    [Undocumented]
+    public enum ObjectAttributes : UInt32
+    {
+        None = 0,
+        Inherit = 0x0002,
+		Permanent = 0x0010,
+        Exclusive = 0x0020,
+		CaseInsensitive = 0x0040,
+		OpenIf = 0x0080,
+		OpenLink = 0x0100,
+        Kernel = 0x0200,
+		ForceAccessCheck = 0x0400
 	}
 }
