af0bcfb50434221d9c7b7459334ca9028426ba68
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2023-05-18 22:54:52 +0200

    Ditch CustomTypeDescriptor in favor of the base interface

--- CdControl/Fields2PropsConverter.cs
+++ CdControl/Fields2PropsConverter.cs
@@ -2,17 +2,16 @@
 using System;
 using System.ComponentModel;
 using System.Reflection;
-using System.Linq;
 
 namespace CdControl {
-	internal class Fields2PropsConverter : CustomTypeDescriptor {
+	internal class Fields2PropsConverter : ICustomTypeDescriptor {
 		private object realObj;
 
 		public Fields2PropsConverter(object realObj) {
 			this.realObj = realObj;
 		}
 
-		public override PropertyDescriptorCollection GetProperties(Attribute[] attributes) {
+		public PropertyDescriptorCollection GetProperties() {
 			var realType = realObj.GetType();
 
 			List<PropertyDescriptor> props = new List<PropertyDescriptor>();
@@ -27,10 +26,19 @@ namespace CdControl {
 
 			return new PropertyDescriptorCollection(props.ToArray());
 		}
+		public PropertyDescriptorCollection GetProperties(Attribute[] attributes) => GetProperties();
 
-		public override string GetClassName() => realObj.GetType().FullName;
+		public EventDescriptor GetDefaultEvent() => TypeDescriptor.GetDefaultEvent(realObj);
+		public TypeConverter GetConverter() => TypeDescriptor.GetConverter(realObj);
+		public AttributeCollection GetAttributes() => TypeDescriptor.GetAttributes(realObj);
+		public EventDescriptorCollection GetEvents() => TypeDescriptor.GetEvents(realObj);
+		public EventDescriptorCollection GetEvents(Attribute[] attributes) => TypeDescriptor.GetEvents(realObj, attributes);
+		public object GetEditor(Type editorBaseType) => TypeDescriptor.GetEditor(realObj, editorBaseType);
+		public string GetComponentName() => TypeDescriptor.GetComponentName(realObj);
+		public PropertyDescriptor GetDefaultProperty() => TypeDescriptor.GetDefaultProperty(realObj);
+		public string GetClassName() => TypeDescriptor.GetClassName(realObj);
 
-		public override object GetPropertyOwner(PropertyDescriptor pd) => realObj;
+		public object GetPropertyOwner(PropertyDescriptor pd) => realObj;
 
 		private class DummyPropDescriptor : PropertyDescriptor {
 			private FieldInfo fieldInfo;
